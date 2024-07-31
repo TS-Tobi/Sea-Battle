831930fe13573219a3aa8441dcc511b15ffe4aa4
Tobias Schneider <tb_schneider@web.de>
2024-07-31 15:35:13 +0200

    Added all exisitng Themes

---
+++ Sea-Battle/Sea-Battle/DropdownMenu.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sea_Battle
+{
+    /// <summary>
+    /// Führen Sie die Schritte 1a oder 1b und anschließend Schritt 2 aus, um dieses benutzerdefinierte Steuerelement in einer XAML-Datei zu verwenden.
+    ///
+    /// Schritt 1a) Verwenden des benutzerdefinierten Steuerelements in einer XAML-Datei, die im aktuellen Projekt vorhanden ist.
+    /// Fügen Sie dieses XmlNamespace-Attribut dem Stammelement der Markupdatei 
+    /// an der Stelle hinzu, an der es verwendet werden soll:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Sea_Battle"
+    ///
+    ///
+    /// Schritt 1b) Verwenden des benutzerdefinierten Steuerelements in einer XAML-Datei, die in einem anderen Projekt vorhanden ist.
+    /// Fügen Sie dieses XmlNamespace-Attribut dem Stammelement der Markupdatei 
+    /// an der Stelle hinzu, an der es verwendet werden soll:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:Sea_Battle;assembly=Sea_Battle"
+    ///
+    /// Darüber hinaus müssen Sie von dem Projekt, das die XAML-Datei enthält, einen Projektverweis
+    /// zu diesem Projekt hinzufügen und das Projekt neu erstellen, um Kompilierungsfehler zu vermeiden:
+    ///
+    ///     Klicken Sie im Projektmappen-Explorer mit der rechten Maustaste auf das Zielprojekt und anschließend auf
+    ///     "Verweis hinzufügen"->"Projekte"->[Navigieren Sie zu diesem Projekt, und wählen Sie es aus.]
+    ///
+    ///
+    /// Schritt 2)
+    /// Fahren Sie fort, und verwenden Sie das Steuerelement in der XAML-Datei.
+    ///
+    ///     <MyNamespace:DropdownMenu/>
+    ///
+    /// </summary>
+    public class DropdownMenu : ContentControl
+    {
+        /*
+         DropdownMenu is a custom ContentControl class that represents a drop-down menu.
+         It contains a DependencyProperty 'IsOpen' that controls the opened or closed state of the menu.
+         */
+
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(DropdownMenu),
+                new PropertyMetadata(false));
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        static DropdownMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DropdownMenu), new FrameworkPropertyMetadata(typeof(DropdownMenu)));
+        }
+    }
+}
